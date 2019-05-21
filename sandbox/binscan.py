6acae5000985dbbef2f95ce89270c5af5b251c11
Doug Davis <ddavis@ddavis.io>
2019-05-21 19:59:38 -0400

    binscan.py update

--- sandbox/binscan.py
+++ sandbox/binscan.py
@@ -5,12 +5,15 @@ import itertools
 import subprocess
 import WtStat.ntuple_trex
 
-ntupdir = "/Users/ddavis/ATLAS/data/bdt_main"
+
+#ntupdir = "/Users/ddavis/ATLAS/data/bdt_main"
+ntupdir = "/home/ddavis/ATLAS/data/bdt_main"
 lumi = 140.5
 name = "tW"
-ncpu = 8
+ncpu = 4
 
-master_3jpT_file = "/Users/ddavis/ATLAS/analysis/WtAna/WtStat/sandbox/tW_reg3jpT_histos.root"
+#master_3jpT_file = "/Users/ddavis/ATLAS/analysis/WtAna/WtStat/sandbox/tW_reg3jpT_histos.root"
+master_3jpT_file = "/home/ddavis/ATLAS/analysis/WtAna/WtStat/sandbox/tW_reg3jpT_histos.root"
 
 pre_constants = []
 pre_constants.append(WtStat.ntuple_trex.Job(name, ntupdir, lumi))
@@ -179,59 +182,13 @@ Systematic: "B_ev_L_0"
   Samples: tW,ttbar
   WeightDown: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_Light_0_down
 
-Systematic: "Jet_BJES_Response"
-  Category: "Jets"
-  NtupleFileSufDown: _JET_CategoryReduction_JET_BJES_Response__1down.bdt_response
-  Title: "JetBJESResponse"
+Systematic: "B_ev_B_0"
+  Category: "WeightBTag"
+  WeightUp: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_B_0_up
+  Title: "b-tag eigenv B0"
   Symmetrisation: TWOSIDED
   Samples: tW,ttbar
-  NtupleNameUp: WtTMVA_JET_CategoryReduction_JET_BJES_Response__1up
-  NtupleFileSufUp: _JET_CategoryReduction_JET_BJES_Response__1up.bdt_response
-  NtupleNameDown: WtTMVA_JET_CategoryReduction_JET_BJES_Response__1down
-
-Systematic: "Jet_EffNP_Modelling2"
-  Category: "Jets"
-  NtupleFileSufDown: _JET_CategoryReduction_JET_EffectiveNP_Modelling2__1down.bdt_response
-  Title: "JetEffNPModelling2"
-  Symmetrisation: TWOSIDED
-  Samples: tW,ttbar
-  NtupleNameUp: WtTMVA_JET_CategoryReduction_JET_EffectiveNP_Modelling2__1up
-  NtupleFileSufUp: _JET_CategoryReduction_JET_EffectiveNP_Modelling2__1up.bdt_response
-  NtupleNameDown: WtTMVA_JET_CategoryReduction_JET_EffectiveNP_Modelling2__1down
-
-Systematic: "Jet_JER_EffectiveNP_7restTerm"
-  Category: "JER"
-  Title: "JetJEREffectiveNP7restTerm"
-  Symmetrisation: ONESIDED
-  Samples: tW,ttbar
-  NtupleNameUp: WtTMVA_JET_CategoryReduction_JET_JER_EffectiveNP_7restTerm__1up
-  NtupleFileSufUp: _JET_CategoryReduction_JET_JER_EffectiveNP_7restTerm__1up.bdt_response
-
-Systematic: "Pileup"
-  Category: "Weights"
-  WeightUp: weight_sys_pileup_UP
-  Title: "Pileup"
-  Symmetrisation: TWOSIDED
-  Samples: tW,ttbar
-  WeightDown: weight_sys_pileup_DOWN
-
-Systematic: "Jet_JER_EffNP_4"
-  Category: "JER"
-  Title: "JetJEREffNP4"
-  Symmetrisation: ONESIDED
-  Samples: tW,ttbar
-  NtupleNameUp: WtTMVA_JET_CategoryReduction_JET_JER_EffectiveNP_4__1up
-  NtupleFileSufUp: _JET_CategoryReduction_JET_JER_EffectiveNP_4__1up.bdt_response
-
-Systematic: "Jet_EtaIntercal_TotalStat"
-  Category: "Jets"
-  NtupleFileSufDown: _JET_CategoryReduction_JET_EtaIntercalibration_TotalStat__1down.bdt_response
-  Title: "JetEtaIntercalTotalStat"
-  Symmetrisation: TWOSIDED
-  Samples: tW,ttbar
-  NtupleNameUp: WtTMVA_JET_CategoryReduction_JET_EtaIntercalibration_TotalStat__1up
-  NtupleFileSufUp: _JET_CategoryReduction_JET_EtaIntercalibration_TotalStat__1up.bdt_response
-  NtupleNameDown: WtTMVA_JET_CategoryReduction_JET_EtaIntercalibration_TotalStat__1down
+  WeightDown: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_B_0_down
 
 Systematic: "B_ev_B_2"
   Category: "WeightBTag"
@@ -241,198 +198,47 @@ Systematic: "B_ev_B_2"
   Samples: tW,ttbar
   WeightDown: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_B_2_down
 
-Systematic: "B_ev_B_2"
-  Category: "WeightBTag"
-  WeightUp: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_B_2_up
-  Title: "b-tag eigenv B2"
-  Symmetrisation: TWOSIDED
-  Samples: tW,ttbar
-  WeightDown: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_B_2_down
-
-Systematic: "B_ev_B_6"
-  Category: "WeightBTag"
-  WeightUp: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_B_6_up
-  Title: "b-tag eigenv B6"
-  Symmetrisation: TWOSIDED
-  Samples: tW,ttbar
-  WeightDown: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_B_6_down
-
-Systematic: "B_ev_B_7"
-  Category: "WeightBTag"
-  WeightUp: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_B_7_up
-  Title: "b-tag eigenv B7"
-  Symmetrisation: TWOSIDED
-  Samples: tW,ttbar
-  WeightDown: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_B_7_down
-
-Systematic: "B_ev_B_1"
-  Category: "WeightBTag"
-  WeightUp: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_B_1_up
-  Title: "b-tag eigenv B1"
-  Symmetrisation: TWOSIDED
-  Samples: tW,ttbar
-  WeightDown: weight_sys_bTagSF_MV2c10_Continuous_eigenvars_B_1_down
-
-Systematic: "Jet_Pileup_OffsetMu"
-  Category: "Jets"
-  NtupleFileSufDown: _JET_CategoryReduction_JET_Pileup_OffsetMu__1down.bdt_response
-  Title: "JetPileupOffsetMu"
-  Symmetrisation: TWOSIDED
-  Samples: tW,ttbar
-  NtupleNameUp: WtTMVA_JET_CategoryReduction_JET_Pileup_OffsetMu__1up
-  NtupleFileSufUp: _JET_CategoryReduction_JET_Pileup_OffsetMu__1up.bdt_response
-  NtupleNameDown: WtTMVA_JET_CategoryReduction_JET_Pileup_OffsetMu__1down
-
-Systematic: "Jet_EffNP_Mixed2"
-  Category: "Jets"
-  NtupleFileSufDown: _JET_CategoryReduction_JET_EffectiveNP_Mixed2__1down.bdt_response
-  Title: "JetEffNPMixed2"
-  Symmetrisation: TWOSIDED
-  Samples: tW,ttbar
-  NtupleNameUp: WtTMVA_JET_CategoryReduction_JET_EffectiveNP_Mixed2__1up
-  NtupleFileSufUp: _JET_CategoryReduction_JET_EffectiveNP_Mixed2__1up.bdt_response
-  NtupleNameDown: WtTMVA_JET_CategoryReduction_JET_EffectiveNP_Mixed2__1down
 """
 
 post_constants = "".join(post_constants)
 post_constants = "{}\n{}".format(post_constants, other_sys)
 
 
-def gen_combos(lo, hi, n):
-    coms = [(lo, hi)]
-    zs, zb = lo + 1, hi - 1
-    while zs <= hi and zb >= lo:
-        if zs > zb:
-            pass
-        else:
-            coms.append((zs, zb))
-        zs += n
-        zb -= n
-    #coms.append((hi, lo))
-    return coms
+bdtv = range(22, 33, 4)
+ptv = [26, 28, 30, 32]
+r2j2b_max = [0.90]
 
-combos_1j1b = gen_combos(0, 23, 1)
-combos_2j1b = gen_combos(0, 23, 1)
-combos_2j2b = gen_combos(0, 27, 1)
+b1j1b = reversed([34, 36, 38])
+b2j1b = reversed([24, 22, 20])
+b2j2b = reversed([34, 35, 38])
+b3jpT = reversed([34, 36, 38])
 
-bash_script = """
-#!/bin/bash
+bash_script = """#!/bin/bash
 
-wt-stat ntupling -c fit.conf -r reg1j1b reg2j1b reg2j2b -m 4
+wt-stat ntupling -c fit.conf -r reg1j1b reg2j1b reg2j2b reg3jpT -m 4
 trex-fitter wdf fit.conf
 """
 
-for i, combo in enumerate(combos_1j1b):
-    opt_1j1b = combo
+
+for i, combo in enumerate(itertools.product(b1j1b, b2j1b, b2j2b, b3jpT, r2j2b_max)):
+    n1j1b, n2j1b, n2j2b, n3jpT, max2j2b = combo
     midblocks = []
-    midblocks.append(WtStat.ntuple_trex.Region_1j1b(23, -0.70, 0.60, zs=opt_1j1b[0], zb=opt_1j1b[1]))
-    midblocks.append(WtStat.ntuple_trex.Region_2j1b(23, -0.70, 0.75))
-    midblocks.append(WtStat.ntuple_trex.Region_2j2b(27, -0.85, 0.95))
-    midblocks.append(WtStat.ntuple_trex.Region_3jpT(30))
+    midblocks.append(WtStat.ntuple_trex.Region_1j1b(n1j1b, -0.70, 0.60))
+    midblocks.append(WtStat.ntuple_trex.Region_2j1b(n2j1b, -0.70, 0.75))
+    midblocks.append(WtStat.ntuple_trex.Region_2j2b(n2j2b, -0.85, max2j2b))
+    midblocks.append(WtStat.ntuple_trex.Region_3jpT(n3jpT))
 
     config = "{}{}{}".format(pre_constants, "".join(midblocks), post_constants)
 
-    dirname = "fitbinscan_{}_vary1j1b_{}-{}".format(str(i).zfill(4), opt_1j1b[0], opt_1j1b[1])
+    dirname = "third_nbinscan90_{}_{}_{}_{}_{}_{}".format(str(i+108).zfill(4),
+                                                           n1j1b, n2j1b, n2j2b, n3jpT, max2j2b)
     os.mkdir(dirname)
     with open("{}/fit.conf".format(dirname), "w") as f:
         f.write("{}{}{}".format(pre_constants, "".join(midblocks), post_constants))
     with open("{}/run.sh".format(dirname), "w") as f:
-        f.write(bash_script)
+        f.write(bash_script.format(dirname))
 
-    os.mkdir("{}/tW".format(dirname))
-    os.mkdir("{}/tW/Histograms".format(dirname))
-    shutil.copy(master_3jpT_file, "{}/tW/Histograms/tW_reg3jpT_histos.root".format(dirname))
-
-    #if i > 3:
-    #    break
-
-for i, combo in enumerate(combos_2j1b):
-    opt_2j1b = combo
-    midblocks = []
-    midblocks.append(WtStat.ntuple_trex.Region_1j1b(23, -0.70, 0.60))
-    midblocks.append(WtStat.ntuple_trex.Region_2j1b(23, -0.70, 0.75, zs=opt_2j1b[0], zb=opt_2j1b[1]))
-    midblocks.append(WtStat.ntuple_trex.Region_2j2b(27, -0.85, 0.95))
-    midblocks.append(WtStat.ntuple_trex.Region_3jpT(30))
-
-    config = "{}{}{}".format(pre_constants, "".join(midblocks), post_constants)
-
-    dirname = "fitbinscan_{}_vary2j1b_{}-{}".format(str(i).zfill(4), opt_2j1b[0], opt_2j1b[1])
-    os.mkdir(dirname)
-    with open("{}/fit.conf".format(dirname), "w") as f:
-        f.write("{}{}{}".format(pre_constants, "".join(midblocks), post_constants))
-    with open("{}/run.sh".format(dirname), "w") as f:
-        f.write(bash_script)
-
-    os.mkdir("{}/tW".format(dirname))
-    os.mkdir("{}/tW/Histograms".format(dirname))
-    shutil.copy(master_3jpT_file, "{}/tW/Histograms/tW_reg3jpT_histos.root".format(dirname))
-
-    #if i > 3:
-    #    break
-
-for i, combo in enumerate(combos_2j2b):
-    opt_2j2b = combo
-    midblocks = []
-    midblocks.append(WtStat.ntuple_trex.Region_1j1b(23, -0.70, 0.60))
-    midblocks.append(WtStat.ntuple_trex.Region_2j1b(23, -0.70, 0.75))
-    midblocks.append(WtStat.ntuple_trex.Region_2j2b(27, -0.85, 0.95, zs=opt_2j2b[0], zb=opt_2j2b[1]))
-    midblocks.append(WtStat.ntuple_trex.Region_3jpT(30))
-
-    config = "{}{}{}".format(pre_constants, "".join(midblocks), post_constants)
-
-
-    dirname = "fitbinscan_{}_vary2j2b_{}-{}".format(str(i).zfill(4), opt_2j2b[0], opt_2j2b[1])
-    os.mkdir(dirname)
-    with open("{}/fit.conf".format(dirname), "w") as f:
-        f.write("{}{}{}".format(pre_constants, "".join(midblocks), post_constants))
-    with open("{}/run.sh".format(dirname), "w") as f:
-        f.write(bash_script)
-
-    os.mkdir("{}/tW".format(dirname))
-    os.mkdir("{}/tW/Histograms".format(dirname))
-    shutil.copy(master_3jpT_file, "{}/tW/Histograms/tW_reg3jpT_histos.root".format(dirname))
-
-    #if i > 3:
-    #    break
-
-items = []
-for item in os.listdir('.'):
-    if 'fitbinscan' in item:
-        items.append(item)
-
-for i, item in enumerate(items):
-    print(i, item)
-    os.chdir(item)
+    os.chdir(dirname)
     subprocess.call("bash run.sh", shell=True)
-    subprocess.call("trex-fitter wdf fit.conf", shell=True)
     os.chdir("..")
-
-################################
-################################
-
-"""
-for i, combo in enumerate(itertools.product(combos_1j1b, combos_2j1b, combos_2j2b)):
-    opt_1j1b, opt_2j1b, opt_2j2b = combo
-
-
-    midblocks = []
-    midblocks.append(WtStat.ntuple_trex.Region_1j1b(23, -0.70, 0.60, zs=opt_1j1b[0], zb=opt_1j1b[1]))
-    midblocks.append(WtStat.ntuple_trex.Region_2j1b(23, -0.70, 0.75, zs=opt_2j1b[0], zb=opt_2j1b[1]))
-    midblocks.append(WtStat.ntuple_trex.Region_2j2b(27, -0.85, 0.95, zs=opt_2j2b[0], zb=opt_2j2b[1]))
-    midblocks.append(WtStat.ntuple_trex.Region_3jpT(30))
-
-    config = "{}{}{}".format(pre_constants, "".join(midblocks), post_constants)
-
-    dirname = "fitbinscan_{}__{}-{}_{}-{}_{}-{}".format(str(i).zfill(4),
-                                                        opt_1j1b[0], opt_1j1b[1],
-                                                        opt_2j1b[0], opt_2j1b[1],
-                                                        opt_2j2b[0], opt_2j2b[1])
-    os.mkdir(dirname)
-    with open("{}/fit.conf".format(dirname), "w") as f:
-        f.write("{}{}{}".format(pre_constants, "".join(midblocks), post_constants))
-    with open("{}/run.sh".format(dirname), "w") as f:
-        f.write(bash_script)
-
-    #if i > 20:
-    #    break
-"""
+    print(i, 81)
